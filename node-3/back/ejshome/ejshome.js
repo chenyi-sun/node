9577a9c22e992dd58fd03151effa62af5a2f9423
/ </>
2018-08-21 16:54:45 +0800

    add ejs parse
    添加 ejs 解析

---
+++ node-3/back/ejshome/ejshome.js
@@ -0,0 +1,10 @@
+var ejs = require("ejs");
+var path = require('path');
+module.exports = function(num, param, req, res){
+      let paths  = path.resolve(__dirname, '../..')
+    //   res.end('jiujiu');
+    //   console.log(paths+param.path);
+      ejs.renderFile(paths+param.path, param.par, function(err, data){
+           param.fun(err, data);
+      });
+}
--- node-3/back/router/actions/login.js
+++ node-3/back/router/actions/login.js
@@ -3,14 +3,22 @@ var path = require('path');
 var url = require("url");
 var common = require("./../../../common.js");
 var root = require("./../../data/static/root.js");
+var ejshome = require("./../../ejshome/ejshome.js");
 
 common = common.common;
-
 module.exports = function(req, res, app){
     if(common.isLogin){ //如果测试设置了默认登录
         req.session.islogin = root.islogin.trueVlue; //赋值系统的登录值
-        console.log(req.session.islogin);
-        res.end('====login test start====');
+        ejshome(1,{
+            path: '/html/login/login.ejs',
+            par: {
+                msg: 'sss',
+                list: 'ssss'
+            },
+            fun: function(err,data){
+                res.end(data);
+            }
+        }, req, res);
     }
     res.end('login is do');
 };
